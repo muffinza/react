206fec1be8c5201fe9803405fa5945fb59009fb7
Muhlan <padcharinsontirot@gmail.com>
2024-03-11 21:29:12 +0700

    finish counter function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const name = "muhlan";
+  const [count, setCount] = useState(0);
+  const counter = () => {
+    setCount(count + 1);
+  };
+  const subtract =()=>{
+    setCount(count - 1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>สวัสดี {name}</h1>
+      <h1> {count} </h1>
+      {/* <button onClick={() => alert("สวัสดี mu")}>คลิก</button> <br /> */}
+      <button onClick={() => counter()}>บวก</button>
+      <button onClick={() => subtract()}>ลบ</button>
+      <button onClick={() => setCount(0)}>รีเซ็ต</button>
     </div>
   );
 }
