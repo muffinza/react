41bded678e0fe85ddecedb62d63cf1b222f72fb0
Muhlan <padcharinsontirot@gmail.com>
2024-06-13 22:37:32 +0700

    update item file

--- src/components/item.js
+++ src/components/item.js
@@ -1,11 +1,15 @@
 export default function Item(props) {
-    const {dataList,deleteStudent} = props
-    
+  const { dataList, deleteStudent } = props;
+
   return (
     <>
-      <li key={dataList.id} style={{color :dataList.species =='ไม่ระบุ'? 'red':'green'}} >
+      <li
+        key={dataList.id}
+        style={{ color: dataList.species == "ไม่ระบุ" ? "red" : "green" }}
+      >
         <span>
-          รหัส  {dataList.id} : ชื่อ  {dataList.name} สายพันธ์ : {dataList.species ? dataList.species : "ไม่ระบุ"}  
+          รหัส {dataList.id} : ชื่อ {dataList.name} สายพันธ์ :{" "}
+          {dataList.species ? dataList.species : "ไม่ระบุ"}
         </span>
         <span>
           <button className="delBtn" onClick={() => deleteStudent(dataList.id)}>
